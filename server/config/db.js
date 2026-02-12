588d4ff06260da5c24874c1a0e5e2588a5c28c3a
Samuel <samu3delgado@gmail.com>
2026-02-11 18:13:50 -0600

    prueba 3
    prueba de prueba 3

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,6 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log(
-      "Intentando conectar con URI:",
-      process.env.MONGODB_URI.substring(0, 20) + "...",
-    );
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
       family: 4, // Force IPv4
       tlsInsecure: true, // DEBUG: Ignore SSL errors
